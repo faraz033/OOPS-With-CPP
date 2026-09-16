641ad64961efb26955b35e3166b36b6b15e3a645
faraz033 <farazfarooqui033@gmail.com>
2026-09-16 10:08:09 +0530

    Ques given on 16 sep

---
+++ 16 September/PatientCounter.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+using namespace std;
+
+class Patient {
+private:
+    int patientId;
+    string patientName;
+
+    static int activePatients;
+
+public:
+    Patient(int id, string name) {
+        patientId = id;
+        patientName = name;
+        activePatients++;
+
+        cout << "Patient created: " << patientName << endl;
+        cout << "Active patients: " << activePatients << endl;
+    }
+
+    ~Patient() {
+        activePatients--;
+
+        cout << "Patient destroyed: " << patientName << endl;
+        cout << "Active patients: " << activePatients << endl;
+    }
+
+    static void showActivePatients() {
+        cout << "Current active patients: " << activePatients << endl;
+    }
+};
+
+int Patient::activePatients = 0;
+
+int main() {
+    Patient::showActivePatients();
+    Patient p1(101, "Faraz");
+
+    {
+        cout << "\nInside local block:" << endl;
+
+        Patient p2(102, "Arush");
+        Patient::showActivePatients();
+
+        Patient p3(103, "Anivaran");
+        Patient::showActivePatients();
+
+        cout << "\nLeaving local block..." << endl;
+    }
+
+    cout << "\nAfter local block:" << endl;
+    Patient::showActivePatients();
+
+    return 0;
+}
